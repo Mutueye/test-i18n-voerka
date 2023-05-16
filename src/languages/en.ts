8a19f33888fdc1e3350429a59aac70c08bb1435c
Mutueye <mutuyueniao@gmail.com>
2023-05-16 18:18:41 +0800

    step4: voerkai18n compile 编译语言包

---
+++ src/languages/en.ts
@@ -0,0 +1,11 @@
+export default {
+    "1": [
+        "You Clicked Once",
+        "You Clicked Twice",
+        "You Clicked {} Times"
+    ],
+    "2": "please input",
+    "3": "Home",
+    "4": "button text",
+    "5": "please select"
+}
--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,36 +1,53 @@
-/* 
-* 注意：执行compile命令会生新后成本文件，所以请不要修改本文件
-* 本文件仅供在执行voerkai18n extract&compile前提供t函数占位引用 
-*/
+/**
+ * 注意：执行compile命令会生新后成本文件，所以请不要修改本文件
+ */
+import messageIds from "./idMap"                                             // 语言ID映射文件
 import runtime from "@voerkai18n/runtime"
 const { translate,VoerkaI18nScope  } = runtime
-
+import defaultFormatters from "./formatters/zh"             // 默认语言格式化器
+const activeFormatters = defaultFormatters
+import defaultMessages from "./zh"  
+const activeMessages = defaultMessages
+ 
+// 语言配置文件
+const scopeSettings = {
+    "languages": [
+        {
+            "name": "zh",
+            "title": "中文"
+        },
+        {
+            "name": "en",
+            "title": "英语"
+        }
+    ],
+    "defaultLanguage": "zh",
+    "activeLanguage": "zh",
+    "namespaces": {}
+}
+const formatters = {
+    'zh' :  defaultFormatters,
+    'en' : ()=>import("./formatters/en")
+}
+// 语言包加载器
+const loaders = { 
+    "en" : ()=>import("./en") 
+}
 
 // 语言作用域
 const scope = new VoerkaI18nScope({
-    id          : "",                    // 当前作用域的id，自动取当前工程的package.json的name
+    ...scopeSettings,                               // languages,defaultLanguage,activeLanguage,namespaces,formatters
+    id          : "test-i18n-voerka",                    // 当前作用域的id，自动取当前工程的package.json的name
     debug       : false,                            // 是否在控制台输出高度信息
-    default     : {},                               // 默认语言包
-    messages    : {},                               // 当前语言包
-    idMap       : {},                               // 消息id映射列表    
-    formatters  : {},                               // 扩展自定义格式化器    
-    loaders     : {},                               // 语言包加载器
-    defaultLanguage: 'zh',                          // 默认语言名称                         
-    activeLanguage: 'zh',                           // 当前语言名称
-    languages: [
-        {
-            name: "zh",
-            title: "中文"
-        },
-        {
-            name: "en",
-            title: "英文"
-        }
-    ]
+    default     : defaultMessages,                  // 默认语言包
+    messages    : activeMessages,                   // 当前语言包
+    idMap       : messageIds,                       // 消息id映射列表    
+    formatters,                                     // 扩展自定义格式化器    
+    loaders                                         // 语言包加载器
 }) 
 // 翻译函数
 const scopedTtranslate = translate.bind(scope) 
 export { 
     scopedTtranslate as t, 
-    scope as i18nScope
+    scope as VoerkaI18nScope
 }
