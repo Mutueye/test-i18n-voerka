1fb2884f2d0e87afdb28ca1d4b335f770f226cfa
Mutueye <mutuyueniao@gmail.com>
2023-05-17 15:49:35 +0800

    Updates

---
+++ assets/en-b2b8d356.js
@@ -0,0 +1,1 @@
+const e={1:["","You have clicked once","You have clicked twice","You have clicked {} times"],2:"please input",3:"Home",4:"button text",5:"please select",6:"I18N example",7:"Click to earn some money",8:"Your balance is: { value | currency }"};export{e as default};
